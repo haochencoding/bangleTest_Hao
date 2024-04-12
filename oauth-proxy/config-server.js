158a3ee531833cdd455c5d744491a9c14a99b8a7
Hao CHEN <hao.chen@cri-paris.org>
2024-04-12 16:13:23 +0200

    Update local domain for config-server.js

--- oauth-proxy/config-server.js
+++ oauth-proxy/config-server.js
@@ -1,5 +1,5 @@
 module.exports = {
   clientId: 'pub_d9b7e3bf71bc47f28c1afd9072b23da6',
   secretKey: 'sec_756a7c8a7e3b428f8bcd318ec46c18b0',
-  domain: 'http://127.0.0.1:5050'
+  domain: 'http://127.0.0.1:5500'
 };
